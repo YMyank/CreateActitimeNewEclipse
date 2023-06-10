dae4fa1939fe745416e107efc53bab11499e0708
MANNU <MANNU@LAPTOP-9V597CQM>
2023-06-10 15:24:05 +0530

    demo git

---
+++ Actitime/src/com/actitime/testscript/DemoGit.java
@@ -0,0 +1,13 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoGit {
+	@Test
+	public void testGit() {
+		Reporter.log("This is demo for git push", true);
+		
+	}
+
+}
